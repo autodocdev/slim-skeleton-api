b7b2a67c6c4f5d4bfd352d95dabf90b6de80ebc8
weliton <welingtonzsilva@gmail.com>
2022-05-24 19:14:39 -0300

    classe refatorada com as regras do phpcs

--- src/Infrastructure/Handlers/Shutdown.php
+++ src/Infrastructure/Handlers/Shutdown.php
@@ -8,30 +8,23 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpInternalServerErrorException;
 use Slim\ResponseEmitter;
 
+use function error_get_last;
+use function ob_clean;
+use function ob_get_length;
+use function sprintf;
+
+use const E_USER_ERROR;
+use const E_USER_NOTICE;
+use const E_USER_WARNING;
+
 class Shutdown
 {
-    /**
-     * @var Request
-     */
-    private $request;
+    private Request $request;
 
-    /**
-     * @var HttpError
-     */
-    private $errorHandler;
+    private HttpError $errorHandler;
 
-    /**
-     * @var bool
-     */
-    private $displayErrorDetails;
+    private bool $displayErrorDetails;
 
-    /**
-     * ShutdownHandler constructor.
-     *
-     * @param Request   $request
-     * @param HttpError $errorHandler
-     * @param bool      $displayErrorDetails
-     */
     public function __construct(Request $request, HttpError $errorHandler, bool $displayErrorDetails)
     {
         $this->request = $request;
@@ -42,44 +35,52 @@ class Shutdown
     public function __invoke()
     {
         $error = error_get_last();
-        if ($error) {
+        if (!$error) {
+            return;
+        }
             $errorFile = $error['file'];
             $errorLine = $error['line'];
             $errorMessage = $error['message'];
             $errorType = $error['type'];
             $message = 'An error while processing your request. Please try again later.';
 
-            if ($this->displayErrorDetails) {
-                switch ($errorType) {
-                    case E_USER_ERROR:
-                        $message = "FATAL ERROR: {$errorMessage}. ";
-                        $message .= " on line {$errorLine} in file {$errorFile}.";
-                        break;
+        if ($this->displayErrorDetails) {
+            switch ($errorType) {
+                case E_USER_ERROR:
+                    $message = sprintf('FATAL ERROR: %s. ', $errorMessage);
+                    $message .= sprintf('on line %s in file %s.', $errorLine, $errorFile);
 
-                    case E_USER_WARNING:
-                        $message = "WARNING: {$errorMessage}";
-                        break;
+                    break;
+                case E_USER_WARNING:
+                    $message = sprintf('WARNING: %s', $errorMessage);
 
-                    case E_USER_NOTICE:
-                        $message = "NOTICE: {$errorMessage}";
-                        break;
+                    break;
+                case E_USER_NOTICE:
+                    $message = sprintf('NOTICE: %s', $errorMessage);
 
-                    default:
-                        $message = "ERROR: {$errorMessage}";
-                        $message .= " on line {$errorLine} in file {$errorFile}.";
-                        break;
-                }
+                    break;
+                default:
+                    $message = sprintf('ERROR: %s', $errorMessage);
+                    $message .= sprintf('on line %s in file %s.', $errorLine, $errorFile);
+
+                    break;
             }
-
-            $exception = new HttpInternalServerErrorException($this->request, $message);
-            $response = $this->errorHandler->__invoke($this->request, $exception, $this->displayErrorDetails, false, false);
-
-            if (ob_get_length()) {
-                ob_clean();
-            }
-
-            $responseEmitter = new ResponseEmitter();
-            $responseEmitter->emit($response);
         }
+
+        $exception = new HttpInternalServerErrorException($this->request, $message);
+        $response = $this->errorHandler->__invoke(
+            $this->request,
+            $exception,
+            $this->displayErrorDetails,
+            false,
+            false
+        );
+
+        if (ob_get_length()) {
+            ob_clean();
+        }
+
+        $responseEmitter = new ResponseEmitter();
+        $responseEmitter->emit($response);
     }
 }
