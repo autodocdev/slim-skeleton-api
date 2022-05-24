2f61f833bc8e912654cf90f2286e88298400eb6a
Diego Rondão <diegorondao86@gmail.com>
2022-05-24 16:05:06 -0300

    feat: add diretório de tests com TestCase

---
+++ tests/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+final class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     * @covers
+     */
+    public function exampleTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
